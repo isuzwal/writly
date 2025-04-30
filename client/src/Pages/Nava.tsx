9341e26e1cce9599514a9ca9f1b3a8279fcb387a
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2025-05-01 00:16:57 +0530

    Change the Navbar

--- client/src/Pages/Nava.tsx
+++ client/src/Pages/Nava.tsx
@@ -1,105 +1,90 @@
 import { Link } from "react-router";
 import { useContext, useState } from "react";
 import { UserContext } from "../UserAuth/User";
-
+import { PenTool } from "lucide-react";
 import { ThemeContex } from "../Theme/Theme";
 
 import { CgMenu } from "react-icons/cg";
 import { IoIosClose } from "react-icons/io";
-function Nava(){
-    const [IsOpen,setIsOpen]=useState<boolean>(false)
-  
-   
-    // checking that User did't return undefine so.
 
-    const { theme, } = useContext(ThemeContex);
- 
-    const context=useContext(UserContext)
-    if(!context){
-      throw new Error
-    }
-    // this for temp will change from backend
-    const {user}=context
+function Nava() {
+  const [IsOpen, setIsOpen] = useState<boolean>(false);
+  const { theme } = useContext(ThemeContex);
+  const context = useContext(UserContext);
   
-    //->Menubar function
-    const ToogleMunebar=()=>{
-        setIsOpen(prev=>!prev)
-    }
- 
-    //for the theme section
-   
-    // for theme
-    const themeStyles = {
-      // dark: "bg-black text-white  ",
-      light: "rgb(254,255,254) text-black   ",
-      // system: "bg-gray-800 text-white ", 
-    };
-    
-   
-    return(
-<section className={` w-full  ${themeStyles[theme as keyof typeof themeStyles]}`}>
-            <div className=" px-1 flex relative items-center justify-between ">
-                <Link to ="/" className="md:text-xl  text-[16px] font-semibold font-dm  whitespace-nowrap flex-shrink-0 ">Writly</Link>
-                 <div className="flex items-center relative px-2 md:w-96 ">
+  if (!context) {
+    throw new Error("User context is undefined");
+  }
+
+  const { user } = context;
+
+  // Toggle function
+  const ToggleMenuBar = () => setIsOpen((prev) => !prev);
+
+  // Theme styles
+  const themeStyles = {
+    light: "bg-white text-black",
+    // Add dark/system themes as needed
+  };
+
+  return (
+    <section className={` bg-navabar w-full ${themeStyles[theme as keyof typeof themeStyles]}`}>
+      <div className=" py-1 px-2 flex relative items-center justify-between">
+        <Link  to="/"
+          className="  text-white hover:bg-gradient-to-t from-slate-100 via-slate-200 to-slate-300 md:text-xl hover:shadow-md   rounded-md py-1 hover:text-writly flex gap-1 text-[16px] font-semibold font-dm px-4 whitespace-nowrap">
+          <PenTool size={20} /> Writly
+        </Link>
+
+      
+        <div className="hidden md:flex items-center gap-4">
+          {user ? (
+            <div onClick={ToggleMenuBar}
+              className={`cursor-pointer flex items-center gap-2 px-4 py-1.5 rounded-lg ${themeStyles[theme as keyof typeof themeStyles]}`}>
+              <img src={user.profileImage} className="object-cover w-7 h-7 rounded-lg" />
+              <span className="font-mono font-medium text-[18px]">{user.username}</span>
+            </div>
+          ) : (
+            <div className="flex items-center gap-3 px-3">
+              <Link to="/login" className="text-[16px]  text-white  hover:bg-zinc-600  hover:bg-opacity-30 font-semibold px-6 py-1 rounded-md hover:bg- transition">
+                Login
+              </Link>
+              <Link to="/register" className="text-[16px] font-semibold text-white px-6 py-1 rounded-md hover:bg-zinc-600  hover:bg-opacity-30 transition">
+                Register
+              </Link>
+            </div>
+          )}
+        </div>
+        <div className="md:hidden p-1 rounded-full">
+          <button onClick={ToggleMenuBar} className="flex items-center">
+            {IsOpen ? (
+              <IoIosClose className=" text-white transition-transform duration-300 hover:rotate-180 hover:scale-110" size={24} />
+            ) : (
+              <CgMenu className=" text-white transition-transform duration-300 hover:rotate-180 hover:scale-110" size={23} />
+            )}
+          </button>
+        </div>
+
+        {IsOpen && (
+          <div className={`absolute bg-navabar top-[40px] right-[1px] w-full p-2 rounded-b-lg transition-all duration-300 ease-in-out ${themeStyles[theme as keyof typeof themeStyles]}`}>
+            {user ? (
+              <div className="flex flex-col items-center gap-2 px-2 py-3">
+                <div className="w-full h-20 rounded">
+                  <img src={user.CoverImage} className="object-cover w-full h-full rounded" />
+                </div>
+                <img src={user.profileImage} className="absolute left-2 w-20 rounded-lg object-cover h-full" />
+                <span className="font-mono font-medium text-lg">{user.username}</span>
               </div>
-                 <div className={`rounded-lg px-6  hidden md:flex   ${themeStyles[theme as keyof typeof themeStyles]}`}>
-                   <div className="flex w-full   rounded-md">
-                  {user? (
-                      <div
-                      className="flex gap-2 justify-between ">
-                        <div  onClick={ToogleMunebar}
-                        className={` flex gap-2 justify-between    px-4 py-1.5 cursor-pointer rounded-lg   ${themeStyles[theme as keyof typeof themeStyles]}`}>
-                      <img src={user.profileImage} className="object-cover w-7 h-7   rounded-lg"  />    
-                      <span className="font-mono  font-medium text-[18px]">{(user?.username)}</span>
-                      </div>
-                      </div>
-                  ):(
-                    <div className="flex items-center   gap-3 px-3 ">
-                   <Link to="/login" className=" text-center text-[16px] font-semibold  px-6
-                     py-1 rounded-md  hover:bg-slate-100 transition-all duration-200">Login</Link>
-                   <Link to="/register"  className=" text-center text-[16px] font-semibold hover:bg-slate-800  bg-black px-6 
-                    text-white py-1 rounded-md  transition-all duration-200">Register</Link>
-                  </div>
-                  )}
-                   </div>
-                 </div>
-                 <div className="items-center md:hidden   p-1 rounded-full " >
-                   <button onClick={ToogleMunebar} className="flex items-center">
-                     {IsOpen ?(
-                       <IoIosClose  className=" transition-transform duration-300  hover:rotate-180 hover:scale-110"  size={24}/>
-                      ):(
-                      <CgMenu className=" transition-transform duration-300  hover:rotate-180 hover:scale-110" size={23} />
-                      )}
-             
-                    </button>
-                 </div>
-                 {IsOpen && (
-                      <div className={` 
-                       ${themeStyles[theme as keyof typeof themeStyles]} md:hidden  absolute  bg-white z-10  top-[30px] w-full right-[1px]  transition-all duration-300 ease-in-out`}>
-                        {user?(
-                          <div className=" flex  flex-col rounded-md   gap-2  px-2 cursor-pointer relative  py-1.5">
-                            <div className="flex  relative flex-col items-center  gap-2   px-2  py-3 rounded-md">
-                             <div className=" w-full h-20  rounded">
-                             <img src={user.CoverImage} className="object-cover w-full h-full  rounded "  />    
-                              </div>
-                              <div className=" absolute  z-30 rounded-r-2xl left-2 ">
-                              <img src={user.profileImage} className="  rounded-r-2xl object-cover w-20 h-full  "  />  
-                              </div>
-                            </div>
-                            
-                         </div>  
-                        ):(
-                          <div className="flex flex-col  gap-4 px-4 py-2 ">
-                            <Link to="/login" className="w-48 text-center text-[16px] font-semibold    hover:bg-slate-200 py-2 rounded-md  transition-all duration-200">Login</Link>
-                            <Link to="/register"  className="w-48 text-center text-[16px] font-semibold hover:bg-black hover:text-white py-2 rounded-md  transition-all duration-200">Register</Link>
-                         </div>
-
-                        )}
-                    </div>
-                 )}
-                 </div>
-           
-       </section>
-    )
+            ) : (
+              <div className="flex flex-col gap-4 px-4 py-2">
+                <Link to="/login" className="text-[16px] rounded-md  text-white  hover:bg-zinc-600  hover:bg-opacity-30 font-semibold  px-2 py-2">Login</Link>
+                <Link to="/register" className=" text-[16px]  rounded-md  text-white  hover:bg-zinc-600  hover:bg-opacity-30 font-semibold  px-2 py-2">Register</Link>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+    </section>
+  );
 }
+
 export default Nava;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -9,7 +9,8 @@ export default {
       colors: {
         profile: "rgb(27,30,38)",
         writly: "rgb(0,1,254)",
-        maincolor:"rgb(29,28,28)"
+        maincolor:"rgb(29,28,28)",
+        navabar:"rgb(36,39,41)"
       },
       fontFamily:{
         sans: ["DM Sans", "sans-serif"], 
