352fb040d06e0f3e4e1a527b1441066a94e500f0
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2025-05-28 04:12:47 +0530

    Comment out the Store for now

--- client/src/State/hook.ts
+++ client/src/State/hook.ts
@@ -1,7 +1,7 @@
-import { useDispatch,useSelector } from "react-redux";
-import type { AppDispatch,RootState } from "./Store";
+// import { useDispatch,useSelector } from "react-redux";
+// import type { AppDispatch,RootState } from "./Store";
 
 
-// normal useDispatch && useSlector in the end is same as  in js 
-export const useAppDispatch=useDispatch.withTypes<AppDispatch>();
-export const useAppSelector=useSelector.withTypes<RootState>();
+// // normal useDispatch && useSlector in the end is same as  in js 
+// export const useAppDispatch=useDispatch.withTypes<AppDispatch>();
+// export const useAppSelector=useSelector.withTypes<RootState>();
