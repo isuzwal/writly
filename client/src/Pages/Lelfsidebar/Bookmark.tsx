37377348125a1721e07f36b09b7d13037f2fcc3d
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2025-06-03 23:28:10 +0530

    Fix

--- client/src/Pages/Lelfsidebar/Bookmark.tsx
+++ client/src/Pages/Lelfsidebar/Bookmark.tsx
@@ -3,7 +3,7 @@ const Bookmark=()=>{
 <section className="min-h-screen text-white  flex flex-col justify-center items-center text-center p-4">
   <h1 className="text-2xl font-semibold mb-2"> Currently in Building Mode – There May Be Bugs 🛠️ </h1>
   <p className="mb-4"> This project is still under development. If you notice anything broken or have ideas to improve it, please let me know!</p>
-   <p className="text-sm flex gap-2  text-gray-500"> Your feedback is valuable and helps make this better for everyone 🙌 
+   <p className="text-sm flex items-center  gap-2  text-gray-500"> Your feedback is valuable and helps make this better for everyone 🙌 
         <a href="https://forms.gle/StCLyu8YV8kVc4js6" target="_blank"  rel="noopener noreferrer" 
         className="text-blue-500 underline mt-4">
         Share Your Feedback
